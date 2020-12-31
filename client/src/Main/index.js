9e2ab25ba5d5eacb4a8f952164ceceec010a0a18
Andrew Odewahn <odewahn@oreilly.com>
2020-12-31 12:02:25 -0500

    added material-ui

--- client/src/Main/index.js
+++ client/src/Main/index.js
@@ -1,7 +1,20 @@
 import "./index.css";
+import { Button } from "@material-ui/core";
 
 const Main = () => {
-  return <h1>Hello, Andrew!</h1>;
+  return (
+    <div>
+      <h1>Hello, Andrew!</h1>
+      <img
+        width="480"
+        src="http://localhost:5000/screenshot?url=https://www.oreilly.com"
+      ></img>
+      <br />
+      <Button variant="contained" color="primary">
+        Click me!
+      </Button>
+    </div>
+  );
 };
 
 export default Main;
